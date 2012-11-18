81e9cd9f0aee0a346b8205b8100c90120a54e547
Alexander Toresson <alexander.toresson@gmail.com>
2012-11-18 16:55:58 +0100

    Implement test cases for all implemented functions

--- test.cpp
+++ test.cpp
@@ -21,6 +21,14 @@ namespace glsl {
 		EXPECT_EQ(a, vec4(1.0f, vec3(2.0f, 3.0f, 4.0f)));
 	}
 	
+	TEST(VecTest, HandlesComparison) {
+		vec4 a(1.0f, 2.0f, 3.0f, 4.0f), b(vec2(1.0f, 2.0f), vec2(3.0f, 4.0f)), c(5.0f, 6.0f, 7.0f, 8.0f);
+		EXPECT_EQ(true, a == b);
+		EXPECT_EQ(true, !(a != b));
+		EXPECT_EQ(true, !(a == c));
+		EXPECT_EQ(true, a != c);
+	}
+
 	TEST(VecTest, HandlesAssignment) {
 		vec4 a(1.0f, 2.0f, 3.0f, 4.0f), b = a;
 		vec3 c;
@@ -96,6 +104,65 @@ namespace glsl {
 		EXPECT_EQ(vec4(-8.0f, -14.0f, -10.0f, -13.0f), a);
 	}
 
+	TEST(VecTest, HandlesTrigonometryFunctions) {
+		float pi=3.1415926535f;
+		vec4 a(0, pi/6, pi/4, pi/3);
+		vec4 res_cos(1.0, sqrt(3)/2, sqrt(2)/2, 1.0/2);
+		vec4 res_sin(0.0, 1.0/2, sqrt(2)/2, sqrt(3)/2);
+
+		// EXPECT_FLOAT_EQ can't take vecs as arguments, 'pre-process' with length()
+		EXPECT_FLOAT_EQ(length(res_cos), length(cos(a)));
+		EXPECT_FLOAT_EQ(length(res_sin), length(sin(a)));
+	}
+
+	TEST(VecTest, HandlesExponentialFunctions) {
+		vec4 a(1.0f, 1.1f, 1.0f, 1.1f), b(1.0f, 1.0f, 1.1f, 1.1f);
+		vec4 res_pow(1.0f, 1.1f, 1.0f, 1.110534241054576f);
+		EXPECT_FLOAT_EQ(length(res_pow), length(pow(a, b)));
+	}
+
+	TEST(VecTest, HandlesCommonFunctions) {
+		vec3 a(1.0f, 2.0f, 3.0f), b(4.0f, 5.0f, 6.0f);
+
+		EXPECT_EQ(2.0f, max(2.0f, 1.0f));
+		EXPECT_EQ(b, max(a, b));
+		EXPECT_EQ(1.0f, min(2.0f, 1.0f));
+		EXPECT_EQ(a, min(a, b));
+		EXPECT_EQ(1.0f, abs(1.0f));
+		EXPECT_EQ(1.0f, abs(-1.0f));
+		EXPECT_EQ(a, abs(a));
+		EXPECT_EQ(a, abs(0.0f-a)); // TODO: Change to -a when negation is implemented
+		EXPECT_EQ(0.0f, clamp(-1.0f, 0.0f, 1.0f));
+		EXPECT_EQ(0.5f, clamp(0.5f, 0.0f, 1.0f));
+		EXPECT_EQ(1.0f, clamp(1.5f, 0.0f, 1.0f));
+		EXPECT_EQ(vec3(0.1f, 0.5f, 0.9f), clamp(vec3(-1.0f, 0.5f, 1.5f), vec3(0.1f, 0.0f, 0.0f), vec3(1.0f, 1.0f, 0.9f)));
+		EXPECT_EQ(vec3(0.0f, 0.5f, 1.0f), clamp(vec3(-1.0f, 0.5f, 1.5f), 0.0f, 1.0f));
+	}
+
+	TEST(VecTest, HandlesGeometricFunctions) {
+		vec3 a(1.0f, 2.0f, 3.0f), b(4.0f, 5.0f, 6.0f);
+
+		EXPECT_EQ(vec3(-3.0f, 6.0f, -3.0f), cross(a, b));
+		EXPECT_EQ(32.0f, dot(a, b));
+		EXPECT_EQ(vec3(-255.0f, -318.0f, -381.0f), reflect(a, b));
+		EXPECT_FLOAT_EQ(sqrt(14.0f), length(a));
+		EXPECT_FLOAT_EQ(sqrt(27.0f), distance(a, b));
+		EXPECT_FLOAT_EQ(length(a/sqrt(14.0f)), length(normalize(a)));
+	}
+
+	TEST(VecTest, HandlesMatrixFunctions) {
+		vec3 a(1.0f, 2.0f, 3.0f);
+		vec4 b(4.0f, 5.0f, 6.0f, 7.0f);
+		mat4x3 res(4.0f, 8.0f, 12.0f, 5.0f, 10.0f, 15.0f, 6.0f, 12.0f, 18.0f, 7.0f, 14.0f, 21.0f);
+
+		EXPECT_EQ(res, outerProduct(a, b));
+	}
+
+	TEST(VecTest, HandlesVectorRelationalFunctions) {
+		vec3 a(1.0f, 2.0f, 3.0f), b(4.0f, 5.0f, 6.0f);
+		EXPECT_EQ(bvec3(true, true, true), greaterThan(b, a));
+	}
+
 	TEST(MatTest, HandlesConstructors) {
 		vec4 a(1.0f, 2.0f, 3.0f, 4.0f);
 		mat4 b(1.0f), c(a, a, a, a), d(1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f, 13.0f, 14.0f, 15.0f, 16.0f);
@@ -110,6 +177,16 @@ namespace glsl {
 
 		// TODO: Constructors from arbitrary combinations of vectors and scalars,
 		//       constructors from matrices of other sizes.
+	}
+
+	TEST(MatTest, HandlesComparison) {
+		mat2x3 a(1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f),
+		       b(vec3(1.0f, 2.0f, 3.0f), vec3(4.0f, 5.0f, 6.0f)),
+		       c(1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 7.0f);
+		EXPECT_EQ(true, a == b);
+		EXPECT_EQ(true, !(a != b));
+		EXPECT_EQ(true, !(a == c));
+		EXPECT_EQ(true, a != c);
 	}
 
 	TEST(MatTest, HandlesAddition) {
@@ -166,6 +243,13 @@ namespace glsl {
 		EXPECT_EQ(res_vm2, a);
 		e *= e;
 		EXPECT_EQ(res_mm2, e);
+	}
+
+	TEST(MatTest, HandlesMatrixFunctions) {
+		mat4x3 a(1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f);
+		mat3x4 res(1.0f, 4.0f, 7.0f, 10.0f, 2.0f, 5.0f, 8.0f, 11.0f, 3.0f, 6.0f, 9.0f, 12.0f);
+
+		EXPECT_EQ(res, transpose(a));
 	}
 }
 
